3d27f38cf90f7471ed8d5353e465033db0905575
MAJD729 <salilahmajdeddin@gmail.com>
2020-06-24 14:07:54 +0200

    odd.js

---
+++ odd.js
@@ -0,0 +1,23 @@
+// pseudocode.
+// 1- defined a variable called i;
+// 2- for loop to loop through the numbers between 1 and 135.
+// 3- if statement the chek if the number is not divisible by 2.
+// 4- print the result.
+
+// code:
+var i;
+for(i=1; i<=135; i++)
+{if(i % 2 === 1)
+    {console.log(i)}
+}
+
+// Diagram.
+// i = 1 ....result = 1
+// i = 2 ....result = 1 because 2 % 2 =0
+// i = 3 ....result = 1 , 3
+//.
+// i = 5 ....result = 1 , 3 , 5
+//.
+//.
+//.
+// i = 135 ....result = 1 , 3 , 5 ,..... , 135
