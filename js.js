a423a2981bda5aaa80d3862eebef428d4cade828
MAJD729 <salilahmajdeddin@gmail.com>
2020-06-24 13:52:59 +0200

    tester

---
+++ js.js
@@ -0,0 +1,20 @@
+// print 1 - 135
+
+//pseudocode:
+// 1- defined a variable called i.
+// 2- for loop that start from 1 and ends at 135.
+// 3- print the result.
+
+var i;
+for(i=1; i<=135; i++){
+    console.log(i)
+}
+// Diagram:
+// i = 1 ...result = 1
+// i = 2 ...result = 1 , 2
+// i = 3 ...result = 1 , 2 , 3
+// .
+// .
+// .
+// .
+// i = 135 ...result = 1 , 2 , 3 ,......, 135
